a0c63f3753b25b715f5683e5b8f932c7b3bdde4f
Daniel Montgomery <damontgomery@gmail.com>
2013-01-26 11:35:44 -0800

    refactor / strip the content for basic needs

--- js/responsive.js
+++ js/responsive.js
@@ -7,62 +7,6 @@
       if (this.css('display') === 'block'){this.css({'display' : ''});}
       else {this.css({'display' : 'block'});}
     }
-
-    // Mini Menu (Responsive)
-    $('.mm-main-menu').click(function(){
-      $(this).toggleClass('active');
-      $('.region-navigation').responsive_toggle();
-    });
-    
-    $('.mm-sub-menu').click(function(){
-      $(this).toggleClass('active');
-      $('.responsive-mobile .block-menu-block').responsive_toggle();
-    });
-
-    // Footer Menu
-    $('.region-footer h3').click(function(){
-      $(this).toggleClass('active');
-      $(this).parent().parent().find('ul.menu').responsive_toggle();
-    });
-    
-    $('.region-footer h3').first().addClass('first');
-    $('.region-footer h3').last().addClass('last');
-
-    // add hover effects for main menu and sub menu
-    $('li.expanded').not('.active-trail').hoverIntent({
-    //$('.responsive-full li.expanded, .region-navigation li.expanded').hoverIntent({
-      over: function(){
-        //do not run on the main menu when on a phone
-        if ($(window).width() <= 480 && $(this).is('.menu-name-main-menu li.expanded')){return;}
-
-        $(this).children('ul').slideDown('fast');
-        $(this).addClass('active');
-      },
-      out: function(){
-        //do not collapse when on a phone
-        if ($(window).width() <= 480){return;}
-
-        $(this).children('ul').slideUp('slow');
-        $(this).removeClass('active');
-      },
-      timeout: 500
-    });
-
-    // Handle Z-index more immediately
-    $('li.expanded').not('.active-trail').hover(
-      function(){
-        //do not run on the main menu when on a phone
-        if ($(window).width() <= 480 && $(this).is('.menu-name-main-menu li.expanded')){return;}
-
-        $(this).children('ul').css({'z-index' : 1});
-      },
-      function(){
-        //do not collapse when on a phone
-        if ($(window).width() <= 480){return;}
-
-        $(this).children('ul').css({'z-index' : ''});
-      }
-    );
     
     // Scroll to top on load
     window.scrollTo(0, 1);
@@ -104,44 +48,6 @@
     $(window).resize(function(){
       resp_max_height($('.grid-block-wide > .content'));
     });
-
-    /*Menu Resizing*/
-    resize_main_menu = function(){
-      var main_menu = $('.menu-name-main-menu');
-      var level_1_links = $('.region-navigation li.depth-1').children('a');
-      var all_links = $('a', main_menu);
-      
-      //reset font size
-      all_links.css({'font-size' : '' });
-
-      //check if mobile phone (don't resize)
-      if ($(window).width() <= 480){return;}
-
-      menu_fits = false;
-      
-      var total_space, total_width, font_size, new_font_size;
-      
-      while (menu_fits === false){
-        total_space = main_menu.width();
-        total_width = 0;
-        font_size = parseFloat(level_1_links.eq(0).css('fontSize'),10);
-
-        level_1_links.each(function(){
-          total_width += $(this).outerWidth();
-        });
-
-        if (total_width <= total_space){menu_fits = true;}
-        else {
-          new_font_size = font_size - 1;
-          if (new_font_size <= 0){new_font_size = 2; menu_fits = true;}
-          all_links.css({'font-size' : new_font_size + 'px'});
-        }
-
-      }
-    }
-    
-    resize_main_menu();
-    $(window).resize(resize_main_menu);
     
   }); //end of window load
   
