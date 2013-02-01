0f7fa2604a17d779a5e657c9f6d2abc30eeb631b
Daniel Montgomery <damontgomery@gmail.com>
2013-02-01 09:04:23 -0800

    comment mock ups

--- templates/comment.tpl.php
+++ templates/comment.tpl.php
@@ -10,15 +10,30 @@
     
     <div class="user clearfix">
       <?php print $picture ?>
-      <div class="username">
-        <?php 
-          print str_replace(' (not verified)', '', $author);
-        ?>
-      </div>
       
-    </div>
+      <div class="user-labels">
+        <div class="username">
+          <?php print str_replace(' (not verified)', '', $author); ?>
+        </div>
+        <div class="user-tag">
+          Not Without Incident
+        </div>
+        <div class="user-donate">
+          <img src="/sites/all/themes/panda/images/cruiseship.gif" alt="">
+        </div>
+        <div class="user-location">
+          <span class="meta-label">location</span> Neither here nor there in a world of wonder
+        </div>
 
-    
+        <div class="user-accounts">
+        <div class="xbox"><span class="meta-label">XBL</span> PandaEskimo</div>
+        <div class="psn"><span class="meta-label">PSN</span> PandaEskimo</div>
+        <div class="wii"><span class="meta-label">Wii</span> PandaEskimo</div>
+        <div class="steam"><span class="meta-label">Steam</span> PandaEskimo</div>
+      </div>
+
+      </div>
+    </div>
     
     <div class="body">
       <div class="main">
